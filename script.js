9fd9c6f3bc21ddf62be438aa2bb325d1984b7286
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-03-09 03:45:44 -0800

    Implement gridSize change button

--- script.js
+++ script.js
@@ -18,13 +18,23 @@ function newGrid(size) {
     }
     container.appendChild(row);
   }
-}
-
-newGrid(4);
-const squares = document.querySelectorAll('.square');
-squares.forEach(item => {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(item => {
   item.addEventListener('mouseenter', e => {
     const target = e.target;
     target.style.backgroundColor = "blue";
   })
+})
+}
+
+newGrid(4);
+
+const btn = document.querySelector(".button");
+btn.addEventListener('click', e => {
+  let gridSize = prompt("Please enter a grid size: X * X (max 100)");
+  while (gridSize > 100) {
+    gridSize = prompt("Please enter a grid size: X * X (max 100)");
+  }
+  newGrid(gridSize);
 });
+
